425a6b4707a6bb7988628be3ac4676502d013a86
Sparsh Gupta <66106648+SparshOM@users.noreply.github.com>
2023-02-10 17:42:34 +0530

    Node App

--- python-app-starting-setup/rng.py
+++ python-app-starting-setup/rng.py
@@ -7,5 +7,5 @@ if (max_number > min_number):
   print(max_number)
 else:
   print(min_number)
-#  rnd_number = randint(min_number, max_number)
-#  print(rnd_number)
+rnd_number = randint(min_number, max_number)
+print(rnd_number)
