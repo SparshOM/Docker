17a6bd8f72f2df44caaaf116efe06c8b921aa2f7
Sparsh Gupta <66106648+SparshOM@users.noreply.github.com>
2023-02-10 12:01:07 +0530

    parctice

---
+++ python-app-starting-setup/rng.py
@@ -0,0 +1,11 @@
+from random import randint
+
+min_number = int(input('Please enter the min number: '))
+max_number = int(input('Please enter the max number: '))
+
+if (max_number > min_number): 
+  print(max_number)
+else:
+  print(min_number)
+#  rnd_number = randint(min_number, max_number)
+#  print(rnd_number)
